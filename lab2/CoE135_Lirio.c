f07f7ecbdf852074eab1d5b82f1ef32e5c3912ff
NIGEL <nigel.lirio@eee.upd.edu.ph>
2019-09-05 11:22:18 +0800

    Lab3

--- lab2/CoE135_Lirio.c
+++ lab2/CoE135_Lirio.c
@@ -2,10 +2,11 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <termios.h>
 #include <time.h>
 #include <string.h>
 #include <signal.h>
-int  kill_me = 0, time_out = -1, totalq = 0, totalc = 0, d1 = 0, d2 = 0, sum = 0, inp = -1;
+int  kill_me = 0, time_out = -1, totalq = 1, totalc = 0, d1 = 0, d2 = 0, sum = 0, inp = -1;
 void alarm_handler(int sig){
     totalq++;
     d1 = rand() % 10;
@@ -16,7 +17,22 @@ void kill_handler(int sig){
     kill_me = 1;
     
 }
+int key_h(){
+    int ch;
+    struct termios og, new;
+    tcgetattr(0, &og); //grab old terminal i/o settings
+    new = og; //make new settings same as old settings
+    new.c_lflag &= ~ICANON; //disable buffered i/o
+    new.c_lflag &= ECHO; //set echo mode
+    tcsetattr(0, TCSANOW, &new);
+    do{
+        ch = getchar();
+    }while(ch == EOF);
+    tcsetattr(fileno(stdin), TCSANOW, &og);
+    return ch;
+}
 int main(){
+    srand(time(0));
     struct sigaction signalS, killSig;
     memset(&signalS,0,sizeof signalS);
     memset(&killSig,0,sizeof signalS);
@@ -24,7 +40,6 @@ int main(){
     killSig.sa_handler = kill_handler;
     sigaction(SIGINT, &killSig, NULL);
     sigaction(SIGALRM, &signalS, NULL);
-    totalq++;
     d1 = rand() % 10;
     d2 = rand() % 10;
     sum = d1 + d2;
@@ -32,16 +47,27 @@ int main(){
     while (1)
     {
         if(kill_me){
-            printf("Are you sure you wanna die? \n");
-            kill_me = 0;
+            printf("You got %d out of %d questions right. \n", totalc, totalq-1);
+            printf("Are you sure you wanna quit? Press Y/y to quit, ENTER to restart! \n");
             int kinp = 0;
+            int c;
             do{
-
+                c = key_h();
+                if(c == 89 || c == 121){
+                    exit(0);
+                }
+                else if(c == 10){
+                    kill_me = 0;
+                    totalc = 0;
+                    totalq = 0;
+                    time_out = -1;
+                    kinp = 1;
+                }
+                
             }while (!kinp);
-            
-
-            return 0;
+            raise(SIGALRM);
         }
+        
         sum = d1 + d2;
         if(time_out == 0){
             printf("TIMED OUT\n");
@@ -58,7 +84,7 @@ int main(){
             time_out = 1;
             raise(SIGALRM);
         }
-        else if(inp != -1){
+        else if(inp != -1 ){
             printf("Wrong! Answer is: %d\n", sum);
             time_out = 1;
             raise(SIGALRM);
