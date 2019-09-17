c3cc2a96adac0d5f69c43ecb25e11564b00f70d7
NIGEL <nigel.lirio@eee.upd.edu.ph>
2019-09-17 14:27:08 +0800

    PS1

---
+++ ps1/pr.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+void process1(int *x) {
+    *x = 1; 
+}
+void process2() {
+// <YOUR CODE HERE>
+}
+
+int data = 0;
+int main() {
+    pid_t pid = fork();
+    if(pid > 0){
+        wait(NULL);
+        process1(&data);
+    }
+    printf("data = %d\n", data);
+    return 0;
+}
